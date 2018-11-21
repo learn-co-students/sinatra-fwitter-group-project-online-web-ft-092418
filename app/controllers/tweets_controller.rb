b285be6f0320661698b7a1cf2261bef2c87a0141
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-11-21 12:23:13 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,77 @@
 class TweetsController < ApplicationController
 
+  get "/tweets" do
+    @session = session
+  #  binding.pry
+    @tweets = Tweet.all
+    if session["user_id"]
+      erb :"/tweets/index"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/tweets/new" do
+    #binding.pry
+    if Helper.logged_in(session)
+      erb :"/tweets/new"
+    else
+      redirect "/login"
+    end
+  end
+
+
+  post "/tweets" do
+  #  binding.pry
+    tweet = Helper.current_user(session).tweets.build(params)
+    if tweet.save
+      redirect "/tweets"
+    else
+      redirect "/tweets/new"
+    end
+  end
+
+  get "/tweets/:id" do
+    if Helper.logged_in(session)
+      @tweet = Tweet.find_by_id(params[:id])
+      #binding.pry
+      erb :"/tweets/show"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/tweets/:id/edit" do
+    if Helper.logged_in(session)
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :"tweets/edit"
+    else
+      redirect "/login"
+    end
+  end
+
+  patch "/tweets/:id" do
+    tweet = Tweet.find_by_id(params[:id])
+    if tweet
+      tweet.update(params[:tweet]) if tweet.user = Helper.current_user(session)
+    end
+    if tweet.save
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
+
+  delete "/tweets/:id/delete" do
+    tweet = Tweet.find_by_id(params[:id])
+    #binding.pry
+    if tweet.user == User.find_by_id(session["user_id"])
+      tweet.destroy
+    end
+      redirect "/tweets"
+
+
+  end
+
 
 end
